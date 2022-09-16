44c191b53a684e5fae8f8d7fa1f4c4175e279597
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-16 02:10:17 -0700

    Creating LinkedList

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+        public Node head;
+        public void Add(int data)
+        {
+            Node node = new Node(data); //creating an object of Node and pushing the data to that object(node)
+            if (this.head == null)
+            {
+                this.head = node; // if the head is null, the data in node will be pushed to head
+            }
+            else
+            {
+                Node temp = head; //else the head data is pushed to temp. where as head is null. So temp is null.
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"Added {node.data} to the list");
+        }
+        public void Display()
+        {
+            Node temp = this.head; //pushing head data to temp. if head data is null the temp will be null and it will show as empty
+            if (temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null) //if the head has some data it will be pushed to temp and shows the data
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next; //moves to next poniter unless temp is null
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to data structures program using generics");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
         }
     }
 }
